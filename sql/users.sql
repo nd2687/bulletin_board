617bca396fbf615ba2f521812ca7580eb94a0664
** <**>
2017-06-01 16:29:25 +0900

    Set hash password for test user

--- sql/users.sql
+++ sql/users.sql
@@ -7,6 +7,6 @@ CREATE TABLE users (
     PRIMARY KEY (id)
 );
 
-INSERT INTO users (email, password, unsubscribe_flag) VALUES('test1@example.com', 'password', '0');
-INSERT INTO users (email, password, unsubscribe_flag) VALUES('test2@example.com', 'password', '0');
-INSERT INTO users (email, password, unsubscribe_flag) VALUES('test3@example.com', 'password', '1');
+INSERT INTO users (email, password, unsubscribe_flag) VALUES('test1@example.com', '$2y$10$bai1r7LhtBVlOnJg6XRc0O5XW8we020Z68QCHs.O69h2E6Idx5Ehy', '0');
+INSERT INTO users (email, password, unsubscribe_flag) VALUES('test2@example.com', '$2y$10$bai1r7LhtBVlOnJg6XRc0O5XW8we020Z68QCHs.O69h2E6Idx5Ehy', '0');
+INSERT INTO users (email, password, unsubscribe_flag) VALUES('test3@example.com', '$2y$10$bai1r7LhtBVlOnJg6XRc0O5XW8we020Z68QCHs.O69h2E6Idx5Ehy', '1');
